e295fff4c1a5bb6c6e2e7a1f4ad640a9899d46a7
Rosuljon <ikramjanov977@gmail.com>
2022-04-26 10:02:08 +0900

    working on cartItem part

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -8,8 +8,12 @@ const Cart = (props) => {
   const totalAmount = `₩${cartCtx.totalAmount}`;
   const hasItem = cartCtx.items.length > 0;
 
-  const cartItemRemoveHandler = (id) => {};
-  const cartItemAddHandler = (item) => {};
+  const cartItemRemoveHandler = (id) => {
+    cartCtx.removeItem(id);
+  };
+  const cartItemAddHandler = (item) => {
+    cartCtx.addItem({...item, amount: 1});
+  };
   const cartItems = (
     <ul className={classes["cart-items"]}>
       {cartCtx.items.map((item) => (
--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,15 +1,31 @@
-import React, {useContext} from 'react';
+import React, {useContext, useEffect, useState} from 'react';
 import CartIcon from "../Cart/CartIcon";
 import classes from "./HeaderCartButton.module.css";
 import CartContext from '../../store/cart-context';
 const HeaderCartButton = (props) => {
-
+  const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
   const cartCtx = useContext(CartContext);
-  const numberOfCartItems = cartCtx.items.reduce((curNumber, item) =>{
+  const {items} = cartCtx;
+  const numberOfCartItems = items.reduce((curNumber, item) =>{
     return curNumber + item.amount;
   },0);
 
-  return (<button className={classes.button} onClick={props.onClick}>
+  const btnClasses = `${classes.button} ${btnIsHighlighted ? classes.bump : '' }`;
+
+  useEffect(()=>{
+    if(items.length === 0 ){
+      return;
+    }
+    setBtnIsHighlighted(true);
+    const timer = setTimeout(() => {
+      setBtnIsHighlighted(false);
+    }, 300);
+    return () => {
+      clearTimeout(timer);
+    };
+  },[items])
+
+  return (<button className={btnClasses} onClick={props.onClick}>
       <span className={classes.icon}><CartIcon/></span>
       <span>Your Cart</span>
       <span className={classes.badge}>{numberOfCartItems}</span>
